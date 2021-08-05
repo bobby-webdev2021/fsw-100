ba1c255600f9d4037e8f96ed7be3d9e269b427e1
bobby-webdev2021 <85532644+bobby-webdev2021@users.noreply.github.com>
2021-08-05 11:48:31 -0500

    Add files via upload

---
+++ week-2/pnp-creatAndAppendElements/index.js
@@ -0,0 +1,13 @@
+var h=document.createElement("h1");
+var t=document.createTextNode("Welcome to my JS site.");
+h.appendChild(t);
+document.body.appendChild(h);
+
+var p=document.createElement("p");
+var t=document.createTextNode("All of this was created with Javascript.");
+p.appendChild(t);
+document.body.appendChild(p);
+
+
+
+
